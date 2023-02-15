0fd3f9e3ada1e81b962051fbb55e9b862c0ed813
Eric Puentes <eric.david2333@gmail.com>
2023-02-15 15:46:47 -0500

    manager skeleton

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,36 @@
 const std = @import("std");
 
 const command = @import("command.zig");
+const manager = @import("manager.zig");
 
 const debug = std.debug;
 const io = std.io;
+const mem = std.mem;
+const process = std.process;
 
 pub fn main() !void {
     var arena_instance = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     arena_instance.deinit();
     const gpa = arena_instance.allocator();
-
-    _ = try command.run(gpa);
-
-    // var cp = std.ChildProcess.init(&[_][]const u8{ "wget", s }, gpa);
-    // const term = try cp.spawnAndWait();
+    // TODO: Capture the especific error, and change message accordingly.
+    run(gpa) catch {
+        fatal("Something went wrong");
+    };
 }
 
+fn fatal(msg: []const u8) noreturn {
+    std.log.err("Invalid usage\n {s}", .{msg});
+    process.exit(1);
+}
+
+fn run(gpa: mem.Allocator) !void {
+    const cmd = try command.run(gpa);
+    switch (cmd.option) {
+        .install_nerd => |name| try manager.install_nerd(gpa, name),
+        .install_url => |url| try manager.install_url(gpa, url),
+        .install_zip => |path| try manager.install_zip(gpa, path),
+    }
+}
 test {
     @import("std").testing.refAllDecls(@This());
 }
---
+++ src/manager.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const mem = std.mem;
+
+const nerd_url: []const u8 = "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/";
+
+pub fn install_nerd(allocator: mem.Allocator, name: []const u8) !void {
+    const url = try std.fmt.allocPrint(allocator, "{s}{s}.zip", .{ nerd_url, name });
+    _ = url;
+}
+pub fn install_zip(allocator: mem.Allocator, path: []const u8) !void {
+    _ = path;
+    _ = allocator;
+}
+pub fn install_url(allocator: mem.allocator, url: []const u8) !void {
+    _ = url;
+    _ = allocator;
+}
+
+fn download(gpa: mem.Allocator, url: []const u8) ![]const u8 {
+    // TODO: Validate why zig's http client is not working properly
+    // downloading zip files, and replace the `wget` process once is fixed
+
+    var cp = std.ChildProcess.init(&[_][]const u8{ "wget", url }, gpa);
+    const term = try cp.spawnAndWait();
+    _ = term;
+}
